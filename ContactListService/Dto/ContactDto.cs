bd7fff48499d59403bb319af1e55287071280b62
Dinesh Kumar <dotnext24@gmail.com>
2020-09-04 21:43:58 +0530

    Add project files.

---
+++ ContactListService/Dto/ContactDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactListService.Dto
+{
+    public class ContactDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string JobTitle { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public string Comments { get; set; }
+        public DateTime LastDateContacted { get; set; }
+    }
+}
---
+++ ContactListService/Migrations/20200904135523_init_db.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactListService.Migrations
+{
+    public partial class init_db : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contact",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    JobTitle = table.Column<string>(nullable: false),
+                    CompanyName = table.Column<string>(nullable: false),
+                    Phone = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Comments = table.Column<string>(nullable: true),
+                    LastDateContacted = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contact", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contact");
+        }
+    }
+}
